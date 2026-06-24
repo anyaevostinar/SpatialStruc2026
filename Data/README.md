beac528ef4fe12399a811a4eea24db3e0d6bea35
Anya Vostinar <vostinar@carleton.edu>
2026-06-24 11:38:08 -0500

    example files for stress and health spatial tests

--- Data/README.md
+++ Data/README.md
@@ -1,6 +1,5 @@
 This folder contains the configuration and scripts for generating the data. The data itself is too much to fit onto GitHub, but with these two files for each treatment, you should be able to regenerate all data.
 
-To regenerate the data in a folder, first make the executable of Symbulation in SymbulationEmp, then go into the folder of interest and run
-```
-python3 simple_repeat.py
-```
+To regenerate the data in a folder, first make the executable of Symbulation in SymbulationEmp, then go into the folder of interest and either run the slurm script if you have a cluster, or look at the end of the slurm script for the line that starts with `./symbulation_sgp`.
+
+The starter settings for stress runs can be found in this repo: https://github.com/john-k-903849031/ALIFE2026-StressSymbiosis/blob/main/data though some things are now called something else.
---
+++ Data/sample_stress_treatment/run-mutualists-mixed.sh
@@ -0,0 +1,38 @@
+#!/bin/bash --login
+
+## This file runs one experimental condition (i.e. a group of jobs
+## that are the same except for their random seed)
+
+## Email settings (they don't work for our setup)
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=does_not_work@carleton.edu
+
+## Job name settings (These do matter, so UPDATE THEM)
+#SBATCH --job-name=sm
+#SBATCH -o sm%A_%a.out
+
+## Memory requirement in megabytes. You might need to make this bigger.
+#SBATCH --mem-per-cpu=500M
+
+## Launch an array of jobs. This determines your random seeds
+#SBATCH --array=100-129
+
+#SBATCH --nodes=1
+
+cd /Accounts/YOUR_USERNAME/YOUR_GIT_REPO/Data/TODAY_DATE_YOUR_EXPERIMENT_FOLDER
+mkdir YOUR_TREATMENT_NAME
+cd YOUR_TREATMENT_NAME
+
+mkdir ${SLURM_ARRAY_TASK_ID}
+cd ${SLURM_ARRAY_TASK_ID}
+
+cp /Accounts/YOUR_USERNAME/YOUR_GIT_REPO/Data/TODAY_DATE_YOUR_EXPERIMENT_FOLDER/SymSettings.cfg .
+cp /Accounts/YOUR_USERNAME/YOUR_GIT_REPO/SymbulationEmp/symbulation_sgp .
+
+## THIS IS AN EXAMPLE, UPDATE TO CORRECT THINGS
+args=" -START_MOI 1 -GRID 0 -TASK_ENV_CFG_PATH diff-reward-env.json -STRESS_TYPE mutualist \
+   -VERTICAL_TRANSMISSION 1 -HOST_MIN_CYCLES_BEFORE_REPRO 0 -SYM_MIN_CYCLES_BEFORE_REPRO 0 \
+   -PARASITE_NUM_OFFSPRING_ON_STRESS_INTERACTION 0 -FILE_NAME _YOUR_TREAMENT"
+./symbulation_sgp $args -SEED ${SLURM_ARRAY_TASK_ID} > run.log
+
+## Run with sbatch -p facultynode --nodelist=edmonstone2024,margulis2024,carver,lederberg run-mutualists-mixed.sh
