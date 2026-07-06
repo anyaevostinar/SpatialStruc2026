dd65f529a57e009fc0726f7f77a3c3c530c591fa
Gabe-Rose <roseg@carleton.edu>
2026-07-06 15:43:43 -0500

    fixed config

---
+++ Data/26_7_6_stress_treatment_fixed_config/run-nosyms-grid-diff.sh
@@ -0,0 +1,38 @@
+#!/bin/bash --login
+
+## This file runs one experimental condition (i.e. a group of jobs
+## that are the same except for their random seed)
+
+## Email settings (they don't work for our setup)
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=does_not_work@carleton.edu
+
+## Job name settings (These do matter, so UPDATE THEM)
+#SBATCH --job-name=snsgd
+#SBATCH -o snsgd%A_%a.out
+
+## Memory requirement in megabytes. You might need to make this bigger.
+#SBATCH --mem-per-cpu=1000M
+
+## Launch an array of jobs. This determines your random seeds
+#SBATCH --array=100-129
+
+#SBATCH --nodes=1
+
+cd /Accounts/roseg/symbulation/SpatialStruc2026/Data/26_7_6_stress_treatment_fixed_config
+mkdir -p nosyms-grid-diff
+cd nosyms-grid-diff
+
+mkdir ${SLURM_ARRAY_TASK_ID}
+cd ${SLURM_ARRAY_TASK_ID}
+
+cp /Accounts/roseg/symbulation/SpatialStruc2026/Data/26_7_6_stress_treatment_fixed_config/SymSettings.cfg .
+cp /Accounts/roseg/symbulation/SpatialStruc2026/Data/26_7_6_stress_treatment_fixed_config/diff-reward-env.json .
+cp /Accounts/roseg/symbulation/SpatialStruc2026/SymbulationEmp/symbulation_sgp .
+
+## THIS IS AN EXAMPLE, UPDATE TO CORRECT THINGS
+args=" -START_MOI 0 -GRID 1 -HOST_MIN_CYCLES_BEFORE_REPRO 0 -HOST_REPRO_RES 100 \
+-TASK_ENV_CFG_PATH diff-reward-env.json"
+./symbulation_sgp $args -SEED ${SLURM_ARRAY_TASK_ID} > run.log
+
+## Run with sbatch -p facultynode --nodelist=edmonstone2024,margulis2024,carver,lederberg run-nosyms-grid-diff.sh
