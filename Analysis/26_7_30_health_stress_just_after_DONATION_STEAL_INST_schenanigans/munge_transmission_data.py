c59c38e2b201088a2c1433ca4322ae7c482ebf95
Gabe-Rose <roseg@carleton.edu>
2026-08-20 10:50:23 -0500

    transmission

---
+++ Analysis/26_7_30_health_stress_just_after_DONATION_STEAL_INST_schenanigans/munge_transmission_data.py
@@ -0,0 +1,31 @@
+import os.path
+import gzip
+
+folder = '../../Data/26_7_30_health_stress_just_after_DONATION_STEAL_INST_schenanigans/'
+
+treatment_folders = ["health-mutualists", "health-parasites"]
+
+reps = range(100,130)
+header = "uid treatment rep update horiz_attempts horiz_successes vert_attempts vert_successes\n"
+
+outputFileName = "munged_transmission_rates.dat"
+
+outFile = open(outputFileName, 'w')
+outFile.write(header)
+
+for t in treatment_folders:
+    for r in reps:
+        fname = folder + "/"+t+"/" + str(r) + "/Data" + "/TransmissionRates_data.csv"
+        uid = t + "_" + str(r)
+        curFile = open(fname, 'r')
+        for line in curFile:
+            if (line[0] != 'u'):
+                splitline = line.strip().split(',')
+                horiz_attempts = sum(int(splitline[i]) for i in range(1, 11))
+                horiz_successes = sum(int(splitline[i]) for i in range(11, 21))
+                vert_attempts = sum(int(splitline[i]) for i in range(21, 31))
+                vert_successes = sum(int(splitline[i]) for i in range(31, 41))
+                outRow = f'{uid} {t} {r} {splitline[0]} {horiz_attempts} {horiz_successes} {vert_attempts} {vert_successes}\n'
+                outFile.write(outRow)
+        curFile.close()
+outFile.close()
---
+++ Analysis/26_8_5_health_nutrient_control_weak/munge_transmission_data.py
@@ -0,0 +1,31 @@
+import os.path
+import gzip
+
+folder = '../../Data/26_8_5_health_nutrient_control/'
+
+treatment_folders = ["health-mutualists", "health-parasites", "nutrient-parasites", "nutrient-mutualists"]
+
+reps = range(100,130)
+header = "uid treatment rep update horiz_attempts horiz_successes vert_attempts vert_successes\n"
+
+outputFileName = "munged_transmission_rates.dat"
+
+outFile = open(outputFileName, 'w')
+outFile.write(header)
+
+for t in treatment_folders:
+    for r in reps:
+        fname = folder + "/"+t+"/" + str(r) + "/Data" + "/TransmissionRates_data.csv"
+        uid = t + "_" + str(r)
+        curFile = open(fname, 'r')
+        for line in curFile:
+            if (line[0] != 'u'):
+                splitline = line.strip().split(',')
+                horiz_attempts = sum(int(splitline[i]) for i in range(1, 11))
+                horiz_successes = sum(int(splitline[i]) for i in range(11, 21))
+                vert_attempts = sum(int(splitline[i]) for i in range(21, 31))
+                vert_successes = sum(int(splitline[i]) for i in range(31, 41))
+                outRow = f'{uid} {t} {r} {splitline[0]} {horiz_attempts} {horiz_successes} {vert_attempts} {vert_successes}\n'
+                outFile.write(outRow)
+        curFile.close()
+outFile.close()
